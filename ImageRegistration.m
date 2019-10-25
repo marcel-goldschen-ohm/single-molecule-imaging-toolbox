5189cdc89116c5527a70862587f4cd5d99fc9f68
Marcel Goldschen-Ohm <goldschen-ohm@utexas.edu>
2019-10-25 17:39:10 -0500

    Create ImageRegistration.m

---
+++ ImageRegistration.m
@@ -0,0 +1,103 @@
+classdef ImageRegistration
+    %IMAGEREGISTRATION Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        movingImage = []; % (2d image) (for channel at column index)
+        fixedImage = []; % (2d image) (for channel at row index)
+        
+        movingSpots = []; % ([x y] spot locations for channel at column index)
+        fixedSpots = []; % ([x y] spot locations for channel at row index)
+        
+        transformation = []; % (2d transform, e.g. affine2d)
+    end
+    
+    methods
+        function obj = ImageRegistration()
+            %IMAGEREGISTRATION Construct an instance of this class
+        end
+        
+        function registeredImage = getRegisteredImage(obj)
+            registeredImage = imwarp(obj.movingImage, obj.transformation, 'OutputView', imref2d(size(obj.fixedImage)));
+        end
+        
+        function registeredSpots = getRegisteredSpots(obj)
+            registeredSpots = transformPointsForward(obj.transformation, obj.movingSpots);
+        end
+        
+        function registerImages(moving, fixed)
+            if exist('moving', 'var') && ~isempty(moving)
+                obj.movingImage = moving;
+            end
+            if exist('fixed', 'var') && ~isempty(fixed)
+                obj.fixedImage = fixed;
+            end
+            movingReg = registrationEstimatorAppWrapper(imadjust(obj.movingImage), imadjust(obj.fixedImage));
+            if ~isempty(movingReg)
+                obj.transformation = movingReg.Transformation;
+            end
+        end
+        
+%         function registerSpots(moving, fixed)
+%             if exist('moving', 'var') && ~isempty(moving)
+%                 obj.movingSpots = moving;
+%             end
+%             if exist('fixed', 'var') && ~isempty(fixed)
+%                 obj.fixedSpots = fixed;
+%             end
+%             ... % TODO
+%         end
+    end
+    
+    methods (Static)
+        function movingReg = registrationEstimatorAppWrapper(moving, fixed)
+            % This is a wrapper for programatically launching MATLAB's
+            % registrationEstimator app and returning the last image registration
+            % struct exported from the app to the base workspace upon closing the app
+            % window.
+            %
+            % Hint: Inputting imadjust(moving), imadjust(fixed) may be helpful.
+            
+            movingReg = [];
+
+            % launch MATLAB's registrationEstimator app
+            registrationEstimator(moving, fixed);
+
+            % get handle to registrationEstimator uifigure
+            appFigureHandle = gobjects(0);
+            hfigs = findall(groot, 'type', 'Figure');
+            for i = 1:numel(hfigs)
+                if hfigs(i).Name == "moving (Moving Image)  &  fixed (Fixed Image)"
+                    appFigureHandle = hfigs(i);
+                    break
+                end
+            end
+            if isempty(appFigureHandle)
+                return
+            end
+
+            % inform user that they need to export the alignment
+            msgbox({ ...
+                '1. Align images', ...
+                '2. Export to workspace (name whatever)', ...
+                '3. Close registration estimator window', ...
+                '4. Last exported alignment will be returned' ...
+                });
+
+            % get list of base workspace variables
+            vars = evalin('base', 'who()');
+            % block until registrationEstimator uifigure is closed
+            waitfor(appFigureHandle);
+            % get list of base workspace variables
+            newvars = evalin('base', 'who()');
+            % find new base workspace variables (i.e. those exported from
+            % registrationEstimator app)
+            newvars = newvars(~ismember(newvars, vars));
+            if ~isempty(newvars)
+                % return registrationEstimator app export
+                movingReg = evalin('base', newvars{end});
+            end
+        end
+    end
+end
+
