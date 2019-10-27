6c168623c5075d28bc4c10a86264f176c3ed7393
Marcel Goldschen-Ohm <goldschen-ohm@utexas.edu>
2019-10-27 18:35:51 -0500

    Update ImageStack.m

--- ImageStack.m
+++ ImageStack.m
@@ -244,5 +244,28 @@ classdef ImageStack < handle
             end
         end
     end
+    
+    methods(Static)
+        function obj = loadobj(s)
+            if isstruct(s)
+                obj = ImageStack();
+                for prop = fieldnames(obj)
+                    if isfield(s, prop)
+                        try
+                            obj.(prop) = s.(prop);
+                        catch
+                            disp(['!!! ERROR: ' class(obj) ': Failed to load property ' prop]);
+                        end
+                    end
+                end
+                unloadedProps = setdiff(fieldnames(s), fieldnames(obj));
+                if ~isempty(unloadedProps)
+                    disp(['!!! WARNING: ' class(obj) ': Did NOT load invalid properties: ' strjoin(unloadedProps, ',')]);
+                end
+            else
+                obj = s;
+            end
+        end
+    end
 end
 
