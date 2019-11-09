b6f8e7bdb8d6aa0d6cbb317c065f3334537e2630
Marcel Goldschen-Ohm <goldschen-ohm@utexas.edu>
2019-11-08 18:25:52 -0600

    Update Spot.m

--- Spot.m
+++ Spot.m
@@ -1,4 +1,4 @@
-classdef Spot < handle
+classdef (ConstructOnLoad) Spot < handle
     %SPOT Summary of this class goes here
     %   Detailed explanation goes here
     %
@@ -12,7 +12,7 @@ classdef Spot < handle
         xy = [];
         
         % e.g. from regionprops() --> Area, Eccentricity, etc.
-        props = struct();
+        props = struct.empty;
         
         % labels can be used ot group spots
         label = "";
@@ -45,26 +45,9 @@ classdef Spot < handle
         end
     end
     
-    methods(Static)
+    methods (Static)
         function obj = loadobj(s)
-            if isstruct(s)
-                obj = Spot();
-                for prop = fieldnames(obj)
-                    if isfield(s, prop)
-                        try
-                            obj.(prop) = s.(prop);
-                        catch
-                            disp(['!!! ERROR: ' class(obj) ': Failed to load property ' prop]);
-                        end
-                    end
-                end
-                unloadedProps = setdiff(fieldnames(s), fieldnames(obj));
-                if ~isempty(unloadedProps)
-                    disp(['!!! WARNING: ' class(obj) ': Did NOT load invalid properties: ' strjoin(unloadedProps, ',')]);
-                end
-            else
-                obj = s;
-            end
+            obj = Utilities.loadobj(Spot(), s);
         end
     end
 end
