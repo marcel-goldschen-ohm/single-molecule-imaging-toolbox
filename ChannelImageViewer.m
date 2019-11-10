e3c253fc4ffe4c09cbf9c6c84d3ba6c86bceb8c2
Marcel Goldschen-Ohm <goldschen-ohm@utexas.edu>
2019-11-09 21:32:06 -0600

    Update ChannelImageViewer.m

--- ChannelImageViewer.m
+++ ChannelImageViewer.m
@@ -8,9 +8,12 @@ classdef ChannelImageViewer < ImageStackViewer
     properties
         % Channel handle.
         channel = Channel();
-    end
-    
-    properties (Access = private)
+        
+        selectedSpot = Spot.empty;
+        
+        spotMarkers = gobjects(0);
+        
+        selectedSpotMarker = gobjects(0);
     end
     
     methods
@@ -27,6 +30,9 @@ classdef ChannelImageViewer < ImageStackViewer
             end
             
             obj@ImageStackViewer(parent);
+            
+            obj.spotMarkers = scatter(obj.imageAxes, nan, nan, 'ro');
+            obj.selectedSpotMarker = scatter(obj.imageAxes, nan, nan, 'yo');
         end
         
         function set.channel(obj, channel)
@@ -37,6 +43,7 @@ classdef ChannelImageViewer < ImageStackViewer
             else
                 obj.imageStack = ImageStack();
             end
+            obj.updateSpotMarkers();
         end
         
         function idx = getSelectedImageIndex(obj)
@@ -49,14 +56,42 @@ classdef ChannelImageViewer < ImageStackViewer
             end
         end
         
-        function channels = getOtherChannelsInParentExperiment(obj)
-            channels = [];
-            if ~isempty(obj.channel.experiment)
-                channels = setdiff(obj.channel.experiment.channels, obj.channel);
+        function rightClickInImage(obj, x, y)
+            menu = obj.getActionsMenu();
+            menu.Visible = 1;
+        end
+        
+        function selectImage(obj, idx)
+            obj.imageStack = obj.channel.images(idx);
+        end
+        
+        function loadImage(obj, varargin)
+            newImage = ImageStack;
+            newImage.load('', '', [], [], true);
+            obj.channel.images = [obj.channel.images newImage];
+            obj.imageStack = newImage;
+            [~, newImage.label, ~] = fileparts(newImage.filepath);
+            obj.editImageInfo();
+        end
+        
+        function removeImage(obj, idx, ask)
+            if exist('ask', 'var') && ask
+                if questdlg(['Remove image ' char(obj.channel.images(idx).label) '?'], ...
+                        'Remove image?', ...
+                        'OK', 'Cancel', 'Cancel') == "Cancel"
+                    return
+                end
+            end
+            delete(obj.channel.images(idx));
+            obj.channel.images(idx) = [];
+            if isempty(obj.channel.images)
+                obj.imageStack = ImageStack;
+            else
+                obj.imageStack = obj.channel.images(min(idx, numel(obj.channel.images)));
             end
         end
         
-        function rightClickInImage(obj, x, y)
+        function menu = getActionsMenu(obj)
             fig = ancestor(obj.Parent, 'Figure');
             menu = uicontextmenu(fig);
             
@@ -74,7 +109,7 @@ classdef ChannelImageViewer < ImageStackViewer
             
             uimenu(menu, 'Label', 'Edit Info', ...
                 'Separator', 'on', ...
-                'Callback', @obj.editSelectedImageInfo);
+                'Callback', @obj.editImageInfo);
             
             removeImageMenu = uimenu(menu, 'Label', 'Remove', ...
                 'Separator', 'on');
@@ -113,27 +148,30 @@ classdef ChannelImageViewer < ImageStackViewer
                 'Separator', 'on', ...
                 'Callback', @(varargin) obj.findSpots());
             
-            otherChannels = obj.getOtherChannelsInParentExperiment();
-            if ~isempty(otherChannels)
-                alignToMenu = uimenu(menu, 'Label', 'Align To', ...
-                    'Separator', 'on');
-                for channel = otherChannels
-                    alignToChannelMenu = uimenu(alignToMenu, 'Label', channel.label);
-                    uimenu(alignToChannelMenu, 'Label', 'Align Images');
-                    uimenu(alignToChannelMenu, 'Label', 'Align Spots');
-                    uimenu(alignToChannelMenu, 'Label', 'Identity');
-                end
-            end
+            uimenu(menu, 'Label', 'Clear Spots', ...
+                'Separator', 'on', ...
+                'Callback', @(varargin) obj.clearSpots());
+            
+%             otherChannels = obj.getOtherChannelsInExperiment();
+%             if ~isempty(otherChannels)
+%                 alignToMenu = uimenu(menu, 'Label', 'Align To', ...
+%                     'Separator', 'on');
+%                 for channel = otherChannels
+%                     alignToChannelMenu = uimenu(alignToMenu, 'Label', channel.label);
+%                     uimenu(alignToChannelMenu, 'Label', 'Align Images', ...
+%                 'Callback', @(varargin) obj.alignImagesTo(channel));
+%                     uimenu(alignToChannelMenu, 'Label', 'Align Spots', ...
+%                 'Callback', @(varargin) obj.alignSpotsTo(channel));
+%                     uimenu(alignToChannelMenu, 'Label', 'Identical', ...
+%                 'Callback', @(varargin) obj.alignIdenticalTo(channel)));
+%                 end
+%             end
             
             menu.Position(1:2) = get(fig, 'CurrentPoint');
-            menu.Visible = 1;
+            %menu.Visible = 1;
         end
         
-        function selectImage(obj, idx)
-            obj.imageStack = obj.channel.images(idx);
-        end
-        
-        function editSelectedImageInfo(obj, varargin)
+        function editImageInfo(obj, varargin)
             if ~obj.getSelectedImageIndex()
                 return
             end
@@ -148,32 +186,6 @@ classdef ChannelImageViewer < ImageStackViewer
             obj.updateInfoText();
         end
         
-        function loadImage(obj, varargin)
-            newImage = ImageStack;
-            newImage.load('', '', [], [], true);
-            obj.channel.images = [obj.channel.images newImage];
-            obj.imageStack = newImage;
-            [~, newImage.label, ~] = fileparts(newImage.filepath);
-            obj.editSelectedImageInfo();
-        end
-        
-        function removeImage(obj, idx, ask)
-            if exist('ask', 'var') && ask
-                if questdlg(['Remove image ' char(obj.channel.images(idx).label) '?'], ...
-                        'Remove image?', ...
-                        'OK', 'Cancel', 'Cancel') == "Cancel"
-                    return
-                end
-            end
-            delete(obj.channel.images(idx));
-            obj.channel.images(idx) = [];
-            if isempty(obj.channel.images)
-                obj.imageStack = ImageStack;
-            else
-                obj.imageStack = obj.channel.images(min(idx, numel(obj.channel.images)));
-            end
-        end
-        
         function duplicate(obj, frames)
             if ~exist('frames', 'var')
                 frames = [];
@@ -186,64 +198,6 @@ classdef ChannelImageViewer < ImageStackViewer
                 end
             catch
             end
-%             if isempty(obj.imageStack.data)
-%                 errordlg('Requires an image.', 'Duplicate');
-%                 return
-%             end
-%             nframes = obj.imageStack.numFrames();
-%             if nframes > 1
-%                 ok = true;
-%                 if ~exist('frames', 'var') || isempty(frames)
-%                     frames = 1:nframes;
-%                     d = dialog('Name', 'Duplicate');
-%                     d.Position(3) = 200;
-%                     d.Position(4) = 50;
-%                     uicontrol(d, 'Style', 'text', 'String', 'Frames', ...
-%                         'Units', 'normalized', 'Position', [0, 0.6, 0.5, 0.4]);
-%                     uicontrol(d, 'Style', 'edit', 'String', [ num2str(frames(1)) '-' num2str(frames(end))], ...
-%                         'Units', 'normalized', 'Position', [0.5, 0.6, 0.5, 0.4], ...
-%                         'Callback', @setFrames_);
-%                     uicontrol(d, 'Style', 'pushbutton', 'String', 'OK', ...
-%                         'Units', 'normalized', 'Position', [0.1, 0, 0.4, 0.6], ...
-%                         'Callback', @ok_);
-%                     uicontrol(d, 'Style', 'pushbutton', 'String', 'Cancel', ...
-%                         'Units', 'normalized', 'Position', [0.5, 0, 0.4, 0.6], ...
-%                         'Callback', 'delete(gcf)');
-%                     ok = false;
-%                     uiwait(d);
-%                 end
-%                 if ~ok
-%                     return
-%                 end
-%             elseif nframes == 1
-%                 frames = 1;
-%             end
-%             function setFrames_(edit, varargin)
-%                 firstlast = split(edit.String, '-');
-%                 first = str2num(firstlast{1});
-%                 if numel(firstlast) == 2
-%                     last = str2num(firstlast{2});
-%                     frames = max(1, first):min(last, nframes);
-%                 else
-%                     frames = first;
-%                 end
-%             end
-%             function ok_(varargin)
-%                 ok = true;
-%                 delete(d);
-%             end
-%             try
-%                 newImage = ImageStack;
-%                 newImage.data = obj.imageStack.data(:,:,:,frames);
-%                 if numel(frames) > 1
-%                     newImage.label = string(sprintf('%s %d-%d', obj.imageStack.label, frames(1), frames(end)));
-%                 else
-%                     newImage.label = string(sprintf('%s %d', obj.imageStack.label, frames));
-%                 end
-%                 obj.channel.images = [obj.channel.images newImage];
-%                 obj.imageStack = newImage;
-%             catch
-%             end
         end
         
         function zproject(obj, frames, method)
@@ -330,7 +284,86 @@ classdef ChannelImageViewer < ImageStackViewer
         end
         
         function findSpots(obj)
+            if obj.imageStack.numChannels() > 1
+                errordlg('Requires a grayscale intensity image.', 'Find Spots');
+                return
+            end
+            t = obj.getCurrentFrameIndex();
+            if ~t
+                return
+            end
+            im = obj.imageStack.getFrame(t);
+            if islogical(im)
+                props = regionprops(im, 'all');
+                fieldnames(props)
+                nspots = numel(props);
+                obj.channel.spots = Spot.empty;
+                if nspots
+                    obj.channel.spots(nspots) = Spot();
+                    for k = 1:nspots
+                        obj.channel.spots(k).xy = props(k).Centroid;
+                        obj.channel.spots(k).props = props(k);
+                    end
+                end
+            else
+                xy = ImageOps.findMaximaPreview(im, [], [], [], [], obj.imageFrame);
+                if isempty(xy)
+                    obj.imageStack = obj.imageStack;
+                    return
+                end
+                nspots = size(xy,1);
+                obj.channel.spots = Spot.empty;
+                if nspots
+                    obj.channel.spots(nspots) = Spot();
+                    for k = 1:nspots
+                        obj.channel.spots(k).xy = xy(k,:);
+                    end
+                end
+            end
+            obj.selectedSpot = Spot.empty;
+            obj.updateSpotMarkers();
         end
+        
+        function clearSpots(obj)
+            obj.channel.spots = Spot.empty;
+            obj.selectedSpot = Spot.empty;
+            obj.updateSpotMarkers();
+        end
+        
+        function updateSpotMarkers(obj)
+            xy = vertcat(obj.channel.spots.xy);
+            if isempty(xy)
+                obj.spotMarkers.XData = nan;
+                obj.spotMarkers.YData = nan;
+            else
+                obj.spotMarkers.XData = xy(:,1);
+                obj.spotMarkers.YData = xy(:,2);
+            end
+            obj.updateSelectedSpotMarker();
+        end
+        
+        function updateSelectedSpotMarker(obj)
+            if isempty(obj.selectedSpot)
+                obj.selectedSpotMarker.XData = nan;
+                obj.selectedSpotMarker.YData = nan;
+            else
+                obj.selectedSpotMarker.XData = obj.selectedSpot.xy(1);
+                obj.selectedSpotMarker.YData = obj.selectedSpot.xy(2);
+            end
+        end
+        
+%         function alignImagesTo(obj, channel)
+%         end
+%         
+%         function alignSpotsTo(obj, channel)
+%             errordlg('Aligning spots is not yet implemented. Check back soon.', 'COMING SOON');
+%         end
+%         
+%         function alignIdenticalTo(obj, channel)
+%             obj.channel.alignToChannel = channel;
+%             obj.channel.alignment = ImageRegistration;
+%             obj.channel.alignment.transformation = affine2d();
+%         end
     end
 end
 
